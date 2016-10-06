27960de977bc748ebd4c886153fa89f8deab4528
Francisco Machado <franm912@gmail.com>
2016-10-06 23:26:36 +0100

    Setup Twitter OAuth

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+// This App requires process variables:
+// PORT = port number
+// MLUSER = username to connect to the db
+// MLPW = password for the user
+// CONSUMER_KEY = twitter OAuth consumer key
+// CONSUMER_SECRET = twitter OAuth consumer secret
+// SECRET = used to compute session hash
+
 // Server port number
 var port = process.env.PORT || 3000;
 
@@ -13,12 +21,83 @@ app.set('view engine', 'ejs');
 
 // Import mongoose and connect to database
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://' + '' + ':' + '' + '@ds053146.mlab.com:53146/codefiasco-sites');
+mongoose.connect('mongodb://' + process.env.MLUSER + ':' + process.env.MLPW + '@ds053146.mlab.com:53146/codefiasco-sites');
+
+// Set schemas for mongo db
+var Schema = mongoose.Schema;
+
+// Poll schema
+var pollSchema = new Schema({
+    name: String,
+    author: String,
+    options: [{
+        optionName: String,
+        optionVotes: Number 
+    }],
+    votedUsers: [{
+        userIp: String
+    }]
+});
+var Poll = mongoose.model('Poll', pollSchema);
+
+// Setup authentication
+var passport = require('passport');
+var Strategy = require('passport-twitter').Strategy;
+
+passport.use(new Strategy({
+        consumerKey: process.env.CONSUMER_KEY,
+        consumerSecret: process.env.CONSUMER_SECRET,
+        callbackURL: 'https://codefiasco-polls.herokuapp.com/login/twitter/return'
+    },
+    function(token, tokenSecret, profile, cb) {
+        return cb(null, profile);
+    }
+));
+
+// Configure Passport authenticated session persistence
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: process.env.SECRET, resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ** Routes **
 
 // Set root route
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
+
+// Twitter OAuth
+app.get('/login/twitter', passport.authenticate('twitter'));
+
+// Twitter OAuth Callback
+app.get('/login/twitter/return', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+// User Dashboard
+app.get('/dashboard',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('pages/index', { user: req.user });
+  });
 
 // Redirect undefined routes to root
 app.get('*', function (req, res) {
